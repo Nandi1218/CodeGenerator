38fd334a64fa61f67d1c0013280e0d46ea459b82
Kanyicska Nándor <kanyicskan@gmail.com>
2026-03-08 17:43:17 +0100

    Add output directory to .gitignore for build artifacts

--- output/nandi/project/generated/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package nandi.project.generated.repository;
-
-import nandi.project.generated.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
--- output/nandi/project/generated/repository/VideoGameRepository.java
+++
@@ -1,9 +0,0 @@
-package nandi.project.generated.repository;
-
-import nandi.project.generated.model.VideoGame;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VideoGameRepository extends JpaRepository<VideoGame, Long> {
-}
